90a54f85dd723770024044128e800734c3659a7a
Phuoc Nguyen <me@phuoc.ng>
2020-10-15 09:27:17 +0700

    Watermark example

---
+++ add-a-watermark/WaterMarkExample.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { RenderPage, RenderPageProps, Viewer } from '@react-pdf-viewer/core';
+
+import '@react-pdf-viewer/core/styles/index.css';
+
+interface WaterMarkExampleProps {
+    fileUrl: string;
+}
+
+const WaterMarkExample: React.FC<WaterMarkExampleProps> = ({ fileUrl }) => {
+    const renderPage: RenderPage = (props: RenderPageProps) => (
+        <>
+            {props.canvasLayer.children}
+            <div
+                style={{
+                    alignItems: 'center',
+                    display: 'flex',
+                    height: '100%',
+                    justifyContent: 'center',
+                    left: 0,
+                    position: 'absolute',
+                    top: 0,
+                    width: '100%',
+                }
+            }>
+                <div
+                    style={{
+                        color: 'rgba(0, 0, 0, 0.2)',
+                        fontSize: `${8 * props.scale}rem`,
+                        fontWeight: 'bold',
+                        textTransform: 'uppercase',
+                        transform: 'rotate(-45deg)',
+                        userSelect: 'none',
+                    }}
+                >
+                    Draft
+                </div>
+            </div>
+            {props.annotationLayer.children}
+            {props.textLayer.children}
+        </>
+    );
+
+    return (
+        <div style={{ border: '1px solid rgba(0, 0, 0, 0.3)' }}>
+            <Viewer
+                fileUrl={fileUrl}
+                renderPage={renderPage}
+            />
+        </div>
+    );
+};
+
+export default WaterMarkExample;
