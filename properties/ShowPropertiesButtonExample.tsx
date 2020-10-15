c9eaa15724065f68dd181864190542185cdf9f72
Phuoc Nguyen <me@phuoc.ng>
2020-10-15 10:04:00 +0700

    properties plugin examples

---
+++ properties/ShowPropertiesButtonExample.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Viewer } from '@react-pdf-viewer/core';
+import { propertiesPlugin } from '@react-pdf-viewer/properties';
+
+import '@react-pdf-viewer/core/styles/index.css';
+import '@react-pdf-viewer/properties/styles/index.css';
+
+interface ShowPropertiesButtonExampleProps {
+    fileUrl: string;
+}
+
+const ShowPropertiesButtonExample: React.FC<ShowPropertiesButtonExampleProps> = ({ fileUrl }) => {
+    const propertiesPluginInstance = propertiesPlugin();
+    const { ShowPropertiesButton } = propertiesPluginInstance;
+
+    return (
+        <div
+            style={{
+                border: '1px solid rgba(0, 0, 0, 0.3)',
+                display: 'flex',
+                flexDirection: 'column',
+                height: '100%',
+            }}
+        >
+            <div
+                style={{
+                    alignItems: 'center',
+                    backgroundColor: '#eeeeee',
+                    borderBottom: '1px solid rgba(0, 0, 0, 0.1)',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    padding: '4px',
+                }}
+            >
+                <ShowPropertiesButton />
+            </div>
+            <div
+                style={{
+                    flex: 1,
+                    overflow: 'hidden',
+                }}
+            >
+                <Viewer
+                    fileUrl={fileUrl}
+                    plugins={[
+                        propertiesPluginInstance,
+                    ]}
+                />
+            </div>
+        </div>
+    );
+};
+
+export default ShowPropertiesButtonExample;
