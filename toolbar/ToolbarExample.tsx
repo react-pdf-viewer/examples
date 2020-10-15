73014e044eb7d6e6e421a81385e701dbd87e2708
Phuoc Nguyen <me@phuoc.ng>
2020-10-15 10:13:57 +0700

    toolbar plugin examples

---
+++ toolbar/ToolbarExample.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Viewer } from '@react-pdf-viewer/core';
+import { toolbarPlugin } from '@react-pdf-viewer/toolbar';
+
+import '@react-pdf-viewer/core/styles/index.css';
+import '@react-pdf-viewer/toolbar/styles/index.css';
+
+interface ToolbarExampleProps {
+    fileUrl: string;
+}
+
+const ToolbarExample: React.FC<ToolbarExampleProps> = ({ fileUrl }) => {
+    const toolbarPluginInstance = toolbarPlugin();
+    const { Toolbar } = toolbarPluginInstance;
+
+    return (
+        <div
+            style={{
+                border: '1px solid rgba(0, 0, 0, 0.3)',
+                display: 'flex',
+                flexDirection: 'column',
+                height: '100%',
+            }}
+        >
+            <div
+                style={{
+                    alignItems: 'center',
+                    backgroundColor: '#eeeeee',
+                    borderBottom: '1px solid rgba(0, 0, 0, 0.1)',
+                    display: 'flex',
+                    padding: '4px',
+                }}
+            >
+                <Toolbar />
+            </div>
+            <div
+                style={{
+                    flex: 1,
+                    overflow: 'hidden',
+                }}
+            >
+                <Viewer
+                    fileUrl={fileUrl}
+                    plugins={[
+                        toolbarPluginInstance,
+                    ]}
+                />
+            </div>
+        </div>
+    );
+};
+
+export default ToolbarExample;
---
+++ toolbar/ToolbarSlotsExample.tsx
@@ -0,0 +1,97 @@
+import React from 'react';
+import { Viewer } from '@react-pdf-viewer/core';
+import { toolbarPlugin, ToolbarSlot } from '@react-pdf-viewer/toolbar';
+
+import '@react-pdf-viewer/core/styles/index.css';
+import '@react-pdf-viewer/toolbar/styles/index.css';
+
+interface ToolbarSlotsExampleProps {
+    fileUrl: string;
+}
+
+const ToolbarSlotsExample: React.FC<ToolbarSlotsExampleProps> = ({ fileUrl }) => {
+    const toolbarPluginInstance = toolbarPlugin();
+    const { Toolbar } = toolbarPluginInstance;
+
+    return (
+        <div
+            style={{
+                border: '1px solid rgba(0, 0, 0, 0.3)',
+                display: 'flex',
+                flexDirection: 'column',
+                height: '100%',
+            }}
+        >
+            <div
+                style={{
+                    alignItems: 'center',
+                    backgroundColor: '#eeeeee',
+                    borderBottom: '1px solid rgba(0, 0, 0, 0.1)',
+                    display: 'flex',
+                    padding: '4px',
+                }}
+            >
+                <Toolbar>
+                {
+                    (props: ToolbarSlot) => {
+                        const {
+                            CurrentPageInput, Download, EnterFullScreen, GoToNextPage, GoToPreviousPage,
+                            NumberOfPages, Print, ShowSearchPopover, Zoom, ZoomIn,
+                            ZoomOut,
+                        } = props;
+                        return (
+                            <>
+                                <div style={{ padding: '0px 2px' }}>
+                                    <ShowSearchPopover />
+                                </div>
+                                <div style={{ padding: '0px 2px' }}>
+                                    <ZoomOut />
+                                </div>
+                                <div style={{ padding: '0px 2px' }}>
+                                    <Zoom />
+                                </div>
+                                <div style={{ padding: '0px 2px' }}>
+                                    <ZoomIn />
+                                </div>
+                                <div style={{ padding: '0px 2px', marginLeft: 'auto' }}>
+                                    <GoToPreviousPage />
+                                </div>
+                                <div style={{ padding: '0px 2px' }}>
+                                    <CurrentPageInput /> / <NumberOfPages />
+                                </div>
+                                <div style={{ padding: '0px 2px' }}>
+                                    <GoToNextPage />
+                                </div>
+                                <div style={{ padding: '0px 2px', marginLeft: 'auto' }}>
+                                    <EnterFullScreen />
+                                </div>
+                                <div style={{ padding: '0px 2px' }}>
+                                    <Download />
+                                </div>
+                                <div style={{ padding: '0px 2px' }}>
+                                    <Print />
+                                </div>
+                            </>
+                        )
+                    }
+                }
+                </Toolbar>
+            </div>
+            <div
+                style={{
+                    flex: 1,
+                    overflow: 'hidden',
+                }}
+            >
+                <Viewer
+                    fileUrl={fileUrl}
+                    plugins={[
+                        toolbarPluginInstance,
+                    ]}
+                />
+            </div>
+        </div>
+    );
+};
+
+export default ToolbarSlotsExample;
