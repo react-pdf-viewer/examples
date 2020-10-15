551668fc4bf2cd809adf625864235332c297cbf1
Phuoc Nguyen <me@phuoc.ng>
2020-10-15 09:30:07 +0700

    Set the page from last visit

---
+++ set-the-page-from-last-visit-as-the-initial-page/StoreCurrentPageExample.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { PageChangeEvent, Viewer } from '@react-pdf-viewer/core';
+
+import '@react-pdf-viewer/core/styles/index.css';
+
+interface StoreCurrentPageExampleProps {
+    fileUrl: string;
+}
+
+const StoreCurrentPageExample: React.FC<StoreCurrentPageExampleProps> = ({ fileUrl }) => {
+    const handlePageChange = (e: PageChangeEvent) => {
+        localStorage.setItem('current-page', `${e.currentPage}`);
+    };
+
+    const initialPage = localStorage.getItem('current-page')
+        ? parseInt(localStorage.getItem('current-page'), 10)
+        : 0;
+
+    return (
+        <Viewer
+            fileUrl={fileUrl}
+            initialPage={initialPage}
+            onPageChange={handlePageChange}
+        />
+    );
+};
+
+export default StoreCurrentPageExample;
