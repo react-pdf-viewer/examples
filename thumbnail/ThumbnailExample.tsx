09c83536c933c8132293da96a3b44a40c59eea53
Phuoc Nguyen <me@phuoc.ng>
2020-10-15 10:11:42 +0700

    thumbnail plugin example

---
+++ thumbnail/ThumbnailExample.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Viewer } from '@react-pdf-viewer/core';
+import { thumbnailPlugin } from '@react-pdf-viewer/thumbnail';
+
+import '@react-pdf-viewer/core/styles/index.css';
+import '@react-pdf-viewer/thumbnail/styles/index.css';
+
+interface ThumbnailExampleProps {
+    fileUrl: string;
+}
+
+const ThumbnailExample: React.FC<ThumbnailExampleProps> = ({ fileUrl }) => {
+    const thumbnailPluginInstance = thumbnailPlugin();
+    const { Thumbnails } = thumbnailPluginInstance;
+
+    return (
+        <div
+            style={{
+                border: '1px solid rgba(0, 0, 0, 0.3)',
+                display: 'flex',
+                height: '100%',
+            }}
+        >
+            <div
+                style={{
+                    borderRight: '1px solid rgba(0, 0, 0, 0.3)',
+                    overflow: 'auto',
+                    width: '30%',
+                }}
+            >
+                <Thumbnails />
+            </div>
+            <div style={{ flex: 1 }}>
+                <Viewer
+                    fileUrl={fileUrl}
+                    plugins={[
+                        thumbnailPluginInstance,
+                    ]}
+                />
+            </div>
+        </div>
+    );
+};
+
+export default ThumbnailExample;
