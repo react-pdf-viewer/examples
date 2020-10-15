c1bfe644e2f614a7bd8b07c47fa796d90b96b8a7
Phuoc Nguyen <me@phuoc.ng>
2020-10-15 09:28:01 +0700

    Disable text selection example

---
+++ disable-text-selection/RemoveTextLayerExample.tsx
@@ -0,0 +1,101 @@
+import React from 'react';
+import { Viewer, RenderPageProps } from '@react-pdf-viewer/core';
+import { toolbarPlugin, ToolbarSlot } from '@react-pdf-viewer/toolbar';
+
+import '@react-pdf-viewer/core/styles/index.css';
+import '@react-pdf-viewer/toolbar/styles/index.css';
+
+interface RemoveTextLayerExampleProps {
+    fileUrl: string;
+}
+
+const RemoveTextLayerExample: React.FC<RemoveTextLayerExampleProps> = ({ fileUrl }) => {
+    const toolbarPluginInstance = toolbarPlugin();
+    const { Toolbar } = toolbarPluginInstance;
+
+    const renderPage = (props: RenderPageProps) => {
+        return (
+            <>
+                {props.canvasLayer.children}
+                {props.annotationLayer.children}
+            </>
+        );
+    };
+
+    return (
+        <div
+            style={{
+                border: '1px solid rgba(0, 0, 0, 0.3)',
+                display: 'flex',
+                flexDirection: 'column',
+                height: '100%',
+            }}
+        >
+            <div
+                style={{
+                    alignItems: 'center',
+                    backgroundColor: '#eeeeee',
+                    borderBottom: '1px solid rgba(0, 0, 0, 0.1)',
+                    display: 'flex',
+                    padding: '4px',
+                }}
+            >
+                <Toolbar>
+                {
+                    (props: ToolbarSlot) => {
+                        const {
+                            CurrentPageInput, EnterFullScreen, GoToNextPage, GoToPreviousPage,
+                            NumberOfPages, Print, Zoom, ZoomIn,
+                            ZoomOut,
+                        } = props;
+                        return (
+                            <>
+                                <div style={{ padding: '0px 2px' }}>
+                                    <ZoomOut />
+                                </div>
+                                <div style={{ padding: '0px 2px' }}>
+                                    <Zoom />
+                                </div>
+                                <div style={{ padding: '0px 2px' }}>
+                                    <ZoomIn />
+                                </div>
+                                <div style={{ padding: '0px 2px', marginLeft: 'auto' }}>
+                                    <GoToPreviousPage />
+                                </div>
+                                <div style={{ padding: '0px 2px' }}>
+                                    <CurrentPageInput /> / <NumberOfPages />
+                                </div>
+                                <div style={{ padding: '0px 2px' }}>
+                                    <GoToNextPage />
+                                </div>
+                                <div style={{ padding: '0px 2px', marginLeft: 'auto' }}>
+                                    <EnterFullScreen />
+                                </div>
+                                <div style={{ padding: '0px 2px' }}>
+                                    <Print />
+                                </div>
+                            </>
+                        )
+                    }
+                }
+                </Toolbar>
+            </div>
+            <div
+                style={{
+                    flex: 1,
+                    overflow: 'hidden',
+                }}
+            >
+                <Viewer
+                    fileUrl='/assets/pdf-open-parameters.pdf'
+                    renderPage={renderPage}
+                    plugins={[
+                        toolbarPluginInstance,
+                    ]}
+                />
+            </div>
+        </div>
+    );
+};
+
+export default RemoveTextLayerExample;
