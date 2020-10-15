1ef37315c3c19b3bc06a66c5dddc3929bd2a065a
Phuoc Nguyen <me@phuoc.ng>
2020-10-15 09:27:33 +0700

    Localization example

---
+++ use-a-different-language/LocalizationExample.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { LocalizationMap, LocalizationProvider, Viewer } from '@react-pdf-viewer/core';
+import { toolbarPlugin } from '@react-pdf-viewer/toolbar';
+
+import '@react-pdf-viewer/core/styles/index.css';
+import '@react-pdf-viewer/toolbar/styles/index.css';
+
+import vi_VN from './vi_VN.json';
+
+interface LocalizationExampleProps {
+    fileUrl: string;
+}
+
+const LocalizationExample: React.FC<LocalizationExampleProps> = ({ fileUrl }) => {
+    const toolbarPluginInstance = toolbarPlugin();
+    const { Toolbar } = toolbarPluginInstance;
+
+    return (
+        <LocalizationProvider localization={(vi_VN as any) as LocalizationMap}>
+        {
+            (_) => (
+                <div
+                    style={{
+                        border: '1px solid rgba(0, 0, 0, 0.3)',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        height: '100%',
+                    }}
+                >
+                    <div
+                        style={{
+                            alignItems: 'center',
+                            backgroundColor: '#eeeeee',
+                            borderBottom: '1px solid rgba(0, 0, 0, 0.1)',
+                            display: 'flex',
+                            padding: '4px',
+                        }}
+                    >
+                        <Toolbar />
+                    </div>
+                    <div
+                        style={{
+                            flex: 1,
+                            overflow: 'hidden',
+                        }}
+                    >
+                        <Viewer
+                            fileUrl={fileUrl}
+                            plugins={[
+                                toolbarPluginInstance,
+                            ]}
+                        />
+                    </div>
+                </div>
+            )
+        }   
+        </LocalizationProvider>
+    );
+};
+
+export default LocalizationExample;
