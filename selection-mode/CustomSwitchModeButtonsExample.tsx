228863808008935ddef1894ef22c1c815ce0044f
Phuoc Nguyen <me@phuoc.ng>
2020-10-15 10:10:50 +0700

    selection-mode plugin examples

---
+++ selection-mode/CustomSwitchModeButtonsExample.tsx
@@ -0,0 +1,95 @@
+import React from 'react';
+import { Viewer } from '@react-pdf-viewer/core';
+import { RenderSwitchSelectionModeProps, SelectionMode, selectionModePlugin } from '@react-pdf-viewer/selection-mode';
+
+import '@react-pdf-viewer/core/styles/index.css';
+import '@react-pdf-viewer/selection-mode/styles/index.css';
+
+interface CustomSwitchModeButtonsExampleProps {
+    fileUrl: string;
+}
+
+const CustomSwitchModeButtonsExample: React.FC<CustomSwitchModeButtonsExampleProps> = ({ fileUrl }) => {
+    const selectionModePluginInstance = selectionModePlugin();
+    const { SwitchSelectionMode } = selectionModePluginInstance;
+
+    return (
+        <div
+            style={{
+                border: '1px solid rgba(0, 0, 0, 0.3)',
+                display: 'flex',
+                flexDirection: 'column',
+                height: '100%',
+            }}
+        >
+            <div
+                style={{
+                    alignItems: 'center',
+                    backgroundColor: '#eeeeee',
+                    borderBottom: '1px solid rgba(0, 0, 0, 0.1)',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    padding: '4px',
+                }}
+            >
+                <div style={{ padding: '0px 2px' }}>
+                    <SwitchSelectionMode mode={SelectionMode.Hand}>
+                    {
+                        (props: RenderSwitchSelectionModeProps) => (
+                            <button
+                                style={{
+                                    backgroundColor: '#357edd',
+                                    border: 'none',
+                                    borderRadius: '4px',
+                                    color: '#ffffff',
+                                    cursor: 'pointer',
+                                    padding: '8px',
+                                }}
+                                onClick={props.onClick}
+                            >
+                                Hand tool
+                            </button>
+                        )
+                    }
+                    </SwitchSelectionMode>
+                </div>
+                <div style={{ padding: '0px 2px' }}>
+                    <SwitchSelectionMode mode={SelectionMode.Text}>
+                    {
+                        (props: RenderSwitchSelectionModeProps) => (
+                            <button
+                                style={{
+                                    backgroundColor: '#357edd',
+                                    border: 'none',
+                                    borderRadius: '4px',
+                                    color: '#ffffff',
+                                    cursor: 'pointer',
+                                    padding: '8px',
+                                }}
+                                onClick={props.onClick}
+                            >
+                                Text selection tool
+                            </button>
+                        )
+                    }
+                    </SwitchSelectionMode>
+                </div>
+            </div>
+            <div
+                style={{
+                    flex: 1,
+                    overflow: 'hidden',
+                }}
+            >
+                <Viewer
+                    fileUrl={fileUrl}
+                    plugins={[
+                        selectionModePluginInstance,
+                    ]}
+                />
+            </div>
+        </div>
+    );
+};
+
+export default CustomSwitchModeButtonsExample;
---
+++ selection-mode/SwitchSelectionModeButtonsExample.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Viewer } from '@react-pdf-viewer/core';
+import { SelectionMode, selectionModePlugin } from '@react-pdf-viewer/selection-mode';
+
+import '@react-pdf-viewer/core/styles/index.css';
+import '@react-pdf-viewer/selection-mode/styles/index.css';
+
+interface SwitchSelectionModeButtonsExampleProps {
+    fileUrl: string;
+}
+
+const SwitchSelectionModeButtonsExample: React.FC<SwitchSelectionModeButtonsExampleProps> = ({ fileUrl }) => {
+    const selectionModePluginInstance = selectionModePlugin();
+    const { SwitchSelectionModeButton } = selectionModePluginInstance;
+
+    return (
+        <div
+            style={{
+                border: '1px solid rgba(0, 0, 0, 0.3)',
+                display: 'flex',
+                flexDirection: 'column',
+                height: '100%',
+            }}
+        >
+            <div
+                style={{
+                    alignItems: 'center',
+                    backgroundColor: '#eeeeee',
+                    borderBottom: '1px solid rgba(0, 0, 0, 0.1)',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    padding: '4px',
+                }}
+            >
+                <div style={{ padding: '0px 2px' }}>
+                    <SwitchSelectionModeButton mode={SelectionMode.Hand} />
+                </div>
+                <div style={{ padding: '0px 2px' }}>
+                    <SwitchSelectionModeButton mode={SelectionMode.Text} />
+                </div>
+            </div>
+            <div
+                style={{
+                    flex: 1,
+                    overflow: 'hidden',
+                }}
+            >
+                <Viewer
+                    fileUrl={fileUrl}
+                    plugins={[
+                        selectionModePluginInstance,
+                    ]}
+                />
+            </div>
+        </div>
+    );
+};
+
+export default SwitchSelectionModeButtonsExample;
