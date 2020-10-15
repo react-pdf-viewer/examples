ac45422b7f0f6da2dd7dd7a8eb16471706fd6978
Phuoc Nguyen <me@phuoc.ng>
2020-10-15 10:05:23 +0700

    rotate plugin examples

---
+++ rotate/CustomRotateButtonsExample.tsx
@@ -0,0 +1,95 @@
+import React from 'react';
+import { Viewer } from '@react-pdf-viewer/core';
+import { RenderRotateProps, RotateDirection, rotatePlugin } from '@react-pdf-viewer/rotate';
+
+import '@react-pdf-viewer/core/styles/index.css';
+
+interface CustomRotateButtonsExampleProps {
+    fileUrl: string;
+}
+
+const CustomRotateButtonsExample: React.FC<CustomRotateButtonsExampleProps> = ({ fileUrl }) => {
+    const rotatePluginInstance = rotatePlugin();
+    const { Rotate } = rotatePluginInstance;
+
+    return (
+        <div
+            style={{
+                border: '1px solid rgba(0, 0, 0, 0.3)',
+                display: 'flex',
+                flexDirection: 'column',
+                height: '100%',
+            }}
+        >
+            <div
+                style={{
+                    alignItems: 'center',
+                    backgroundColor: '#eeeeee',
+                    borderBottom: '1px solid rgba(0, 0, 0, 0.1)',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    padding: '4px',
+                }}
+            >
+                <div style={{ padding: '0px 2px' }}>
+                    <Rotate direction={RotateDirection.Backward}>
+                    {
+                        (props: RenderRotateProps) => (
+                            <button
+                                style={{
+                                    backgroundColor: '#357edd',
+                                    border: 'none',
+                                    borderRadius: '4px',
+                                    color: '#ffffff',
+                                    cursor: 'pointer',
+                                    padding: '8px',
+                                }}
+                                onClick={props.onClick}
+                            >
+                                Rotate backward
+                            </button>
+                        )
+                    }
+                    </Rotate>
+                </div>
+                <div style={{ padding: '0px 2px' }}>
+                    <Rotate direction={RotateDirection.Forward}>
+                    {
+                        (props: RenderRotateProps) => (
+                            <button
+                                style={{
+                                    backgroundColor: '#357edd',
+                                    border: 'none',
+                                    borderRadius: '4px',
+                                    color: '#ffffff',
+                                    cursor: 'pointer',
+                                    padding: '8px',
+                                }}
+                                onClick={props.onClick}
+                            >
+                                Rotate forward
+                            </button>
+                        )
+                    }
+                    </Rotate>
+                </div>
+                
+            </div>
+            <div
+                style={{
+                    flex: 1,
+                    overflow: 'hidden',
+                }}
+            >
+                <Viewer
+                    fileUrl={fileUrl}
+                    plugins={[
+                        rotatePluginInstance,
+                    ]}
+                />
+            </div>
+        </div>
+    );
+};
+
+export default CustomRotateButtonsExample;
