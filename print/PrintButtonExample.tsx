5add733ee215ea959dbd3b3a6e3818375fd9b1c3
Phuoc Nguyen <me@phuoc.ng>
2020-10-15 10:02:30 +0700

    print plugin examples

---
+++ print/PrintButtonExample.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Viewer } from '@react-pdf-viewer/core';
+import { printPlugin } from '@react-pdf-viewer/print';
+
+import '@react-pdf-viewer/core/styles/index.css';
+import '@react-pdf-viewer/print/styles/index.css';
+
+interface PrintButtonExampleProps {
+    fileUrl: string;
+}
+
+const PrintButtonExample: React.FC<PrintButtonExampleProps> = ({ fileUrl }) => {
+    const printPluginInstance = printPlugin();
+    const { PrintButton } = printPluginInstance;
+
+    return (
+        <div
+            style={{
+                border: '1px solid rgba(0, 0, 0, 0.3)',
+                display: 'flex',
+                flexDirection: 'column',
+                height: '100%',
+            }}
+        >
+            <div
+                style={{
+                    alignItems: 'center',
+                    backgroundColor: '#eeeeee',
+                    borderBottom: '1px solid rgba(0, 0, 0, 0.1)',
+                    display: 'flex',
+                    padding: '4px',
+                }}
+            >
+                <PrintButton />
+            </div>
+            <div
+                style={{
+                    flex: 1,
+                    overflow: 'hidden',
+                }}
+            >
+                <Viewer
+                    fileUrl={fileUrl}
+                    plugins={[
+                        printPluginInstance,
+                    ]}
+                />
+            </div>
+        </div>
+    );
+};
+
+export default PrintButtonExample;
