10570a8f4c9f209bd6c666fac635bb3f68025840
Phuoc Nguyen <me@phuoc.ng>
2020-10-15 09:47:09 +0700

    bookmark plugin example

---
+++ bookmark/BookmarkExample.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { bookmarkPlugin } from '@react-pdf-viewer/bookmark';
+import { Viewer } from '@react-pdf-viewer/core';
+
+import '@react-pdf-viewer/bookmark/styles/index.css';
+import '@react-pdf-viewer/core/styles/index.css';
+
+interface BookmarkExampleProps {
+    fileUrl: string;
+}
+
+const BookmarkExample: React.FC<BookmarkExampleProps> = ({ fileUrl }) => {
+    const bookmarkPluginInstance = bookmarkPlugin();
+
+    const { Bookmarks } = bookmarkPluginInstance;
+
+    return (
+        <div
+            style={{
+                border: '1px solid rgba(0, 0, 0, 0.3)',
+                display: 'flex',
+                height: '100%',
+            }}
+        >
+            <div
+                style={{
+                    borderRight: '1px solid rgba(0, 0, 0, 0.3)',
+                    overflow: 'auto',
+                    width: '30%',
+                }}
+            >
+                <Bookmarks />
+            </div>
+            <div style={{ flex: 1 }}>
+                <Viewer
+                    fileUrl={fileUrl}
+                    plugins={[
+                        bookmarkPluginInstance,
+                    ]}
+                />
+            </div>
+        </div>
+    );
+};
+
+export default BookmarkExample;
