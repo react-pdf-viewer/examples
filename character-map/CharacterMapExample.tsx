f6d683e04d847404183d4c7ad126ab611e889548
Phuoc Nguyen <me@phuoc.ng>
2020-10-18 21:19:23 +0700

    Add characterMap example

---
+++ character-map/CharacterMapExample.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { CharacterMap, Viewer } from '@react-pdf-viewer/core';
+
+interface CharacterMapExampleProps {
+    fileUrl: string;
+}
+
+const CharacterMapExample: React.FC<CharacterMapExampleProps> = ({ fileUrl }) => {
+    const characterMap: CharacterMap = {
+        isCompressed: true,
+        url: 'https://unpkg.com/pdfjs-dist@2.5.207/cmaps/',
+    };
+
+    return (
+        <div
+            style={{
+                border: '1px solid rgba(0, 0, 0, 0.3)',
+                height: '750px'
+            }}
+        >
+            <Viewer
+                characterMap={characterMap}
+                fileUrl={fileUrl}
+            />
+        </div>
+    );
+};
+
+export default CharacterMapExample;
