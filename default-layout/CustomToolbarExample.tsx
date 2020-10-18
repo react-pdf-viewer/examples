11f0818651fb32bb69e02ce744c73f676247701a
Phuoc Nguyen <me@phuoc.ng>
2020-10-18 20:34:13 +0700

    Add custom toolbar example

---
+++ default-layout/CustomToolbarExample.tsx
@@ -0,0 +1,149 @@
+import React, { ReactElement } from 'react';
+import { Viewer } from '@react-pdf-viewer/core';
+import { defaultLayoutPlugin, ToolbarProps, ToolbarSlot } from '@react-pdf-viewer/default-layout';
+
+import '@react-pdf-viewer/core/styles/index.css';
+import '@react-pdf-viewer/default-layout/styles/index.css';
+
+const CustomToolbarExample: React.FC<{}> = () => {
+    const renderToolbar = (Toolbar: ((props: ToolbarProps) => ReactElement)) => (
+        <Toolbar>
+        {
+            (slots: ToolbarSlot) => {
+                const {
+                    CurrentPageInput, CurrentScale, GoToNextPage, GoToPreviousPage,
+                    NumberOfPages, ZoomIn, ZoomOut,
+                } = slots;
+                return (
+                    <div
+                        style={{
+                            alignItems: 'center',
+                            display: 'flex',
+                        }}
+                    >
+                        <div style={{ padding: '0px 2px' }}>
+                            <ZoomOut>
+                            {
+                                (props) => (
+                                    <button
+                                        style={{
+                                            backgroundColor: '#357edd',
+                                            border: 'none',
+                                            borderRadius: '4px',
+                                            color: '#ffffff',
+                                            cursor: 'pointer',
+                                            padding: '8px',
+                                        }}
+                                        onClick={props.onClick}
+                                    >
+                                        Zoom out
+                                    </button>
+                                )
+                            }
+                            </ZoomOut>
+                        </div>
+                        <div style={{ padding: '0px 2px' }}>
+                            <CurrentScale>
+                            {
+                                (props) => (
+                                    <span>{`${Math.round(props.scale * 100)}%`}</span>
+                                )
+                            }
+                            </CurrentScale>
+                        </div>
+                        <div style={{ padding: '0px 2px' }}>
+                            <ZoomIn>
+                            {
+                                (props) => (
+                                    <button
+                                        style={{
+                                            backgroundColor: '#357edd',
+                                            border: 'none',
+                                            borderRadius: '4px',
+                                            color: '#ffffff',
+                                            cursor: 'pointer',
+                                            padding: '8px',
+                                        }}
+                                        onClick={props.onClick}
+                                    >
+                                        Zoom in
+                                    </button>
+                                )
+                            }
+                            </ZoomIn>
+                        </div>
+                        <div style={{ padding: '0px 2px', marginLeft: 'auto' }}>
+                            <GoToPreviousPage>
+                            {
+                                (props) => (
+                                    <button
+                                        style={{
+                                            backgroundColor: props.isDisabled ? '#96ccff' : '#357edd',
+                                            border: 'none',
+                                            borderRadius: '4px',
+                                            color: '#ffffff',
+                                            cursor: props.isDisabled ? 'not-allowed' : 'pointer',
+                                            padding: '8px',
+                                        }}
+                                        disabled={props.isDisabled}
+                                        onClick={props.onClick}
+                                    >
+                                        Previous page
+                                    </button>
+                                )
+                            }
+                            </GoToPreviousPage>
+                        </div>
+                        <div style={{ padding: '0px 2px' }}>
+                            <CurrentPageInput /> / <NumberOfPages />
+                        </div>
+                        <div style={{ padding: '0px 2px' }}>
+                            <GoToNextPage>
+                            {
+                                (props) => (
+                                    <button
+                                        style={{
+                                            backgroundColor: props.isDisabled ? '#96ccff' : '#357edd',
+                                            border: 'none',
+                                            borderRadius: '4px',
+                                            color: '#ffffff',
+                                            cursor: props.isDisabled ? 'not-allowed' : 'pointer',
+                                            padding: '8px',
+                                        }}
+                                        disabled={props.isDisabled}
+                                        onClick={props.onClick}
+                                    >
+                                        Next page
+                                    </button>
+                                )
+                            }
+                            </GoToNextPage>
+                        </div>
+                    </div>
+                )
+            }
+        }
+        </Toolbar>
+    );
+
+    const defaultLayoutPluginInstance = defaultLayoutPlugin({
+        renderToolbar,
+    });
+
+    return (
+        <div
+            style={{
+                height: '100%',
+            }}
+        >
+            <Viewer
+                fileUrl='/assets/pdf-open-parameters.pdf'
+                plugins={[
+                    defaultLayoutPluginInstance,
+                ]}
+            />
+        </div>
+    );
+};
+
+export default CustomToolbarExample;
