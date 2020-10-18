297d5013685c7eeeaeca1b356b768ecc3b17c82f
Phuoc Nguyen <me@phuoc.ng>
2020-10-18 18:18:18 +0700

    Custom loader example

---
+++ customize-loader-renderer/RenderLoaderExample.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { ProgressBar, Viewer } from '@react-pdf-viewer/core';
+
+interface RenderLoaderExampleProps {
+    fileUrl: string;
+}
+
+const RenderLoaderExample: React.FC<RenderLoaderExampleProps> = ({ fileUrl }) => {
+    const renderLoader = (percentages: number) => (
+        <div style={{ width: '240px' }}>
+            <ProgressBar progress={Math.round(percentages)} />
+        </div>
+    );
+
+    return (
+        <div
+            style={{
+                border: '1px solid rgba(0, 0, 0, 0.3)',
+                height: '750px',
+            }}
+        >
+            <Viewer
+                fileUrl={fileUrl}
+                renderLoader={renderLoader}
+            />
+        </div>
+    );
+};
+
+export default RenderLoaderExample;
