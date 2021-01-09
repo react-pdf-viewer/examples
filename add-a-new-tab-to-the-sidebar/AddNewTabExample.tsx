33b948f229987ef509401d9af9ee4ffb61e215cc
Phuoc Nguyen <me@phuoc.ng>
2021-01-09 18:10:52 +0700

    Add a new tab to the sidebar

---
+++ add-a-new-tab-to-the-sidebar/AddNewTabExample.tsx
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import { Icon, Viewer } from '@react-pdf-viewer/core';
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+
+import '@react-pdf-viewer/core/lib/styles/index.css';
+import '@react-pdf-viewer/default-layout/lib/styles/index.css';
+
+interface AddNewTabExampleProps {
+    fileUrl: string;
+}
+
+const AddNewTabExample: React.FC<AddNewTabExampleProps> = ({ fileUrl }) => {
+    const defaultLayoutPluginInstance = defaultLayoutPlugin({
+        sidebarTabs: defaultTabs => defaultTabs.concat({
+            content: (
+                <div style={{ textAlign: 'center', width: '100%' }}>
+                    Notes are listed here
+                </div>
+            ),
+            icon: (
+                <Icon size={16}>
+                    <path d='M23.5,17a1,1,0,0,1-1,1h-11l-4,4V18h-6a1,1,0,0,1-1-1V3a1,1,0,0,1,1-1h21a1,1,0,0,1,1,1Z' />
+                    <path d='M5.5 12L18.5 12' />
+                    <path d='M5.5 7L18.5 7' />
+                </Icon>
+            ),
+            title: <>Notes</>,
+        }),
+    });
+
+    return (
+        <div
+            style={{
+                height: '100%',
+            }}
+        >
+            <Viewer
+                fileUrl={fileUrl}
+                plugins={[
+                    defaultLayoutPluginInstance,
+                ]}
+            />
+        </div>
+    );
+};
+
+export default AddNewTabExample;
