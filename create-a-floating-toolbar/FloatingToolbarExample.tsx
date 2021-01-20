1ff059ccc34b353eb9c6b56e0c5d519de053d73d
Phuoc Nguyen <me@phuoc.ng>
2021-01-20 19:31:11 +0700

    Create a floating toolbar

---
+++ create-a-floating-toolbar/FloatingToolbarExample.tsx
@@ -0,0 +1,96 @@
+import * as React from 'react';
+import { Viewer } from '@react-pdf-viewer/core';
+import { toolbarPlugin, ToolbarSlot } from '@react-pdf-viewer/toolbar';
+
+import '@react-pdf-viewer/core/lib/styles/index.css';
+import '@react-pdf-viewer/toolbar/lib/styles/index.css';
+
+interface FloatingToolbarExampleProps {
+    fileUrl: string;
+}
+
+const FloatingToolbarExample: React.FC<FloatingToolbarExampleProps> = ({ fileUrl }) => {
+    const toolbarPluginInstance = toolbarPlugin();
+    const { Toolbar } = toolbarPluginInstance;
+
+    return (
+        <div
+            style={{
+                border: '1px solid rgba(0, 0, 0, 0.3)',
+                height: '100%',
+                position: 'relative',
+            }}
+        >
+            <div
+                style={{
+                    alignItems: 'center',
+                    backgroundColor: '#eeeeee',
+                    border: '1px solid rgba(0, 0, 0, 0.2)',
+                    borderRadius: '2px',
+                    bottom: '16px',
+                    display: 'flex',
+                    left: '50%',
+                    padding: '4px',
+                    position: 'absolute',
+                    transform: 'translate(-50%, 0)',
+                    zIndex: 1,
+                }}
+            >
+                <Toolbar>
+                {
+                    (props: ToolbarSlot) => {
+                        const {
+                            CurrentPageInput, Download, EnterFullScreen, GoToNextPage, GoToPreviousPage,
+                            NumberOfPages, Print, ZoomIn,
+                            ZoomOut,
+                        } = props;
+                        return (
+                            <>
+                                <div style={{ padding: '0px 2px' }}>
+                                    <ZoomOut />
+                                </div>
+                                <div style={{ padding: '0px 2px' }}>
+                                    <ZoomIn />
+                                </div>
+                                <div style={{ padding: '0px 2px', marginLeft: 'auto' }}>
+                                    <GoToPreviousPage />
+                                </div>
+                                <div style={{ padding: '0px 2px' }}>
+                                    <CurrentPageInput /> / <NumberOfPages />
+                                </div>
+                                <div style={{ padding: '0px 2px' }}>
+                                    <GoToNextPage />
+                                </div>
+                                <div style={{ padding: '0px 2px', marginLeft: 'auto' }}>
+                                    <EnterFullScreen />
+                                </div>
+                                <div style={{ padding: '0px 2px' }}>
+                                    <Download />
+                                </div>
+                                <div style={{ padding: '0px 2px' }}>
+                                    <Print />
+                                </div>
+                            </>
+                        )
+                    }
+                }
+                </Toolbar>
+            </div>
+            <div
+                style={{
+                    flex: 1,
+                    overflow: 'hidden',
+                }}
+            >
+                <Viewer
+                    fileUrl={fileUrl}
+                    plugins={[
+                        toolbarPluginInstance,
+                    ]}
+                />
+            </div>
+        </div>
+    );
+};
+
+export default FloatingToolbarExample;
