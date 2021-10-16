e692d3fb4761d321b016ec91fe686d4e5ba56c3f
Phuoc Nguyen <me@phuoc.ng>
2021-10-16 14:35:20 +0700

    Update Display a thumbnail of the given page example

--- display-a-thumbnail-of-the-given-page/DisplayThumbnailExample.tsx
+++ display-a-thumbnail-of-the-given-page/DisplayThumbnailExample.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { Viewer } from '@react-pdf-viewer/core';
+import { thumbnailPlugin } from '@react-pdf-viewer/thumbnail';
 
 import '@react-pdf-viewer/core/lib/styles/index.css';
 
@@ -11,11 +12,13 @@ interface DisplayThumbnailExampleProps {
 }
 
 const DisplayThumbnailExample: React.FC<DisplayThumbnailExampleProps> = ({ fileUrl, pageIndex }) => {
+    const thumbnailPluginInstance = thumbnailPlugin();
+    const { Cover } = thumbnailPluginInstance;
     const pageThumbnailPluginInstance = pageThumbnailPlugin({
-        pageIndex,
+        PageThumbnail: <Cover getPageIndex={() => pageIndex} />,
     });
 
-    return <Viewer fileUrl={fileUrl} plugins={[pageThumbnailPluginInstance]} />;
+    return <Viewer fileUrl={fileUrl} plugins={[pageThumbnailPluginInstance, thumbnailPluginInstance]} />;
 };
 
 export default DisplayThumbnailExample;
