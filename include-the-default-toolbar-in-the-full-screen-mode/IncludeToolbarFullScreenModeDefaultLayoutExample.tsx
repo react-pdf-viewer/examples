8199287f24abb6ee3ae573e7c2ea890b608270ed
Phuoc Nguyen <me@phuoc.ng>
2022-01-14 09:08:14 +0700

    feat: Include the default toolbar in the full screen mode

---
+++ include-the-default-toolbar-in-the-full-screen-mode/IncludeToolbarFullScreenModeDefaultLayoutExample.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import { Viewer } from '@react-pdf-viewer/core';
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+
+import '@react-pdf-viewer/core/lib/styles/index.css';
+import '@react-pdf-viewer/default-layout/lib/styles/index.css';
+
+interface IncludeToolbarFullScreenModeDefaultLayoutExampleProps {
+    fileUrl: string;
+}
+
+const IncludeToolbarFullScreenModeDefaultLayoutExample: React.FC<IncludeToolbarFullScreenModeDefaultLayoutExampleProps> =
+    ({ fileUrl }) => {
+        const defaultLayoutPluginInstance = defaultLayoutPlugin({
+            toolbarPlugin: {
+                fullScreenPlugin: {
+                    getFullScreenTarget: (pagesContainer) =>
+                        pagesContainer.closest('[data-testid="default-layout__body"]'),
+                    renderExitFullScreenButton: (props) => <></>,
+                },
+            },
+        });
+
+        return <Viewer fileUrl={fileUrl} plugins={[defaultLayoutPluginInstance]} />;
+    };
+
+export default IncludeToolbarFullScreenModeDefaultLayoutExample;
--- include-the-default-toolbar-in-the-full-screen-mode/IncludeToolbarFullScreenModeExample.tsx
+++ include-the-default-toolbar-in-the-full-screen-mode/IncludeToolbarFullScreenModeExample.tsx
@@ -1,25 +1,55 @@
 import * as React from 'react';
 import { Viewer } from '@react-pdf-viewer/core';
-import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+import { toolbarPlugin } from '@react-pdf-viewer/toolbar';
 
 import '@react-pdf-viewer/core/lib/styles/index.css';
-import '@react-pdf-viewer/default-layout/lib/styles/index.css';
+import '@react-pdf-viewer/toolbar/lib/styles/index.css';
 
 interface IncludeToolbarFullScreenModeExampleProps {
     fileUrl: string;
 }
 
 const IncludeToolbarFullScreenModeExample: React.FC<IncludeToolbarFullScreenModeExampleProps> = ({ fileUrl }) => {
-    const defaultLayoutPluginInstance = defaultLayoutPlugin({
-        toolbarPlugin: {
-            fullScreenPlugin: {
-                getFullScreenTarget: (pagesContainer) => pagesContainer.closest('[data-testid="default-layout__body"]'),
-                renderExitFullScreenButton: (props) => <></>,
-            },
+    const toolbarPluginInstance = toolbarPlugin({
+        fullScreenPlugin: {
+            getFullScreenTarget: (pagesContainer) => pagesContainer.closest('.js-viewer-container'),
+            renderExitFullScreenButton: (props) => <></>,
         },
     });
 
-    return <Viewer fileUrl={fileUrl} plugins={[defaultLayoutPluginInstance]} />;
+    const { Toolbar } = toolbarPluginInstance;
+
+    return (
+        <div
+            className="js-viewer-container"
+            style={{
+                border: '1px solid rgba(0, 0, 0, 0.3)',
+                display: 'flex',
+                flexDirection: 'column',
+                height: '100%',
+            }}
+        >
+            <div
+                style={{
+                    alignItems: 'center',
+                    backgroundColor: '#eeeeee',
+                    borderBottom: '1px solid rgba(0, 0, 0, 0.1)',
+                    display: 'flex',
+                    padding: '0.25rem',
+                }}
+            >
+                <Toolbar />
+            </div>
+            <div
+                style={{
+                    flex: 1,
+                    overflow: 'hidden',
+                }}
+            >
+                <Viewer fileUrl={fileUrl} plugins={[toolbarPluginInstance]} />
+            </div>
+        </div>
+    );
 };
 
 export default IncludeToolbarFullScreenModeExample;
